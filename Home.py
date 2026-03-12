666785fc579acc079e3838c186dd751705beb059
PerCaholic23 <xphichnratnakrchwis@gmail.com>
2026-03-12 13:25:48 +0700

    Fix typo
    just a little typo on Home.py nothing much

--- Home.py
+++ Home.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("Enemble & Nueral network by")
+st.title("Ensemble & Neural network by")
 st.header("6704062611182 Chawit Apichonrattanakorn")
 st.header("6704062611026 Satapass Xuto")
